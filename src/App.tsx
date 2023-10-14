0670413f7656cce663b211bbc94fd9b558139e3d
Erick Rodrigues <erick.rod1100@gmail.com>
2023-10-14 00:34:49 -0300

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
+import './index.css'
+import Header from './components/Header';
 import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
+    <div>
+      <Header />
         <h1>PJ x CLT</h1>
-      </div>
-    </>
-  )
+    </div>
+  ) 
 }
 
 export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-neutral-700 py-8">
+      <div className="text-center">
+        <h1 className="text-5xl text-white font-bold">PJ x CLT</h1>
+      </div>
+    </header>
+  )
+};
+
+export default Header;
