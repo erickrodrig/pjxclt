5e990dd3bf287e0fa56c561443a8977169ccd395
Erick Rodrigues <erick.rod1100@gmail.com>
2023-10-15 05:45:33 -0300

    Add dark mode toggle button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,18 @@
+import { useDarkMode } from "../contexts/DarkModeContext";
+import DarkModeIcon from "../assets/icons8-moon-48.png";
+import LightModeIcon from "../assets/icons8-sun.svg";
+
 const Header = () => {
+  const { darkMode, setDarkMode } = useDarkMode();
+
   return (
-    <header className="bg-neutral-700 py-8">
+    <header className="bg-neutral-200 dark:bg-neutral-700 py-8 relative">
+      <button className="absolute right-20 top-1/2 transform -translate-y-1/2 p-2 rounded-md hover:bg-neutral-800 dark:hover:bg-neutral-100 dark:text-white transition"
+        onClick={() => setDarkMode(!darkMode)}>
+        <img src={darkMode ? LightModeIcon : DarkModeIcon} width="32" height="32" title={darkMode ? "Modo claro" : "Modo escuro"} alt="Ativar/desativar modo escuro" />
+      </button>
       <div className="text-center">
-        <h1 className="text-5xl text-white font-bold">PJ x CLT</h1>
+        <h1 className="text-5xl dark:text-white font-bold">PJ x CLT</h1>
       </div>
     </header>
   )
