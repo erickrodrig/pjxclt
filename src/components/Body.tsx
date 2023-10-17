657cc7f36c8d74426f5d081910aca2465e6a3607
Erick Rodrigues <erick.rod1100@gmail.com>
2023-10-17 16:00:21 -0300

    Add flex grow, change body (wip)

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,57 +1,53 @@
 import { useState } from 'react';
 
+function compare() {
+  // logic to compare the PJ and CLT input values and provide results/estimations
+}
+
 const Body = () => {
   const [pjRemuneration, setPJRemuneration] = useState('');
   const [cltRemuneration, setCLTRemuneration] = useState('');
 
   return (
-    <div className="p-20 bg-neutral-800 text-white text-left">
-      <div className="rounded grid grid-cols-2 gap-8 border-8 border-white p-10">
+    <div className="p-20 dark:bg-neutral-800 dark:text-white text-left flex-grow">
+      <div className="rounded grid grid-cols-3 gap-8 border-8 border-white p-10">
+        {/* head */}
+        <div></div> {/* empty div for spacing */}
         <div>
-          <h2 className="text-2xl font-semibold pb-4">PJ</h2>
-          <table className="w-full">
-            <thead>
-              <tr>
-                <th>Remuneração</th>
-                {/* Add other table headers as needed */}
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <input
-                    type="text"
-                    value={pjRemuneration}
-                    onChange={(e) => setPJRemuneration(e.target.value)}
-                  />
-                </td>
-                {/* Add other table row content as needed */}
-              </tr>
-            </tbody>
-          </table>
+          <h2 className="text-2xl font-semibold pb-4 text-center">PJ</h2>
         </div>
         <div>
-          <h2 className="text-2xl font-semibold pb-4">CLT</h2>
-          <table className="w-full">
-            <thead>
-              <tr>
-                <th>Remuneração</th>
-                {/* Add other table headers as needed */}
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <input
-                    type="text"
-                    value={cltRemuneration}
-                    onChange={(e) => setCLTRemuneration(e.target.value)}
-                  />
-                </td>
-                {/* Add other table row content as needed */}
-              </tr>
-            </tbody>
-          </table>
+          <h2 className="text-2xl font-semibold pb-4 text-center">CLT</h2>
+        </div>
+
+        <div className="font-semibold">Remuneração</div>
+        <div className="text-center">
+          <input
+            type="text"
+            value={pjRemuneration}
+            onChange={(e) => setPJRemuneration(e.target.value)}
+            className="border rounded p-1"
+          />
+        </div>
+        <div className="text-center">
+          <input
+            type="text"
+            value={cltRemuneration}
+            onChange={(e) => setCLTRemuneration(e.target.value)}
+            className="border rounded p-1"
+          />
+        </div>
+
+        {/* ... Repeat for others */}
+
+        {/* button */}
+        <div className="col-span-3 text-center mt-8">
+          <button
+            className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={compare}
+          >
+            Compare
+          </button>
         </div>
       </div>
     </div>
