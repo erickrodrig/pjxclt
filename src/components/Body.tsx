35bdfa31ddd76d4aae945ccc6177a5e8d8a76a42
Erick Rodrigues <erick.rod1100@gmail.com>
2023-10-23 21:31:52 -0300

    Added logo

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -7,7 +7,7 @@ interface BodyProps {
 const Body = ({ children }: BodyProps) => {
   return (
     <div className="p-20 dark:bg-neutral-800 dark:text-white text-left flex-grow">
-      <div className="rounded-xl bg-slate-100 dark:bg-neutral-700 p-10">
+      <div className="rounded-xl bg-slate-100 dark:bg-neutral-700 p-10 w-3/5 mx-auto">
         {children}
       </div>
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,17 @@
 import { useDarkMode } from "../contexts/DarkModeContext";
 import DarkModeIcon from "../assets/images/icons8-moon-48.png";
 import LightModeIcon from "../assets/images/icons8-sun.svg";
+import ProfitIcon from "../assets/images/profit.png";
 
 const Header = () => {
   const { darkMode, setDarkMode } = useDarkMode();
 
   return (
     <div className="bg-slate-100 dark:bg-neutral-700 py-8 flex justify-between items-center pl-20 space-x-4 pr-5 sm:pr-20">
-      <h1 className="font-medium text-4xl dark:text-white">PJ x CLT</h1>
+      <div className="inline-flex">
+        <img src={ProfitIcon} width="64" height="64" alt="ícone de uma calculadora e uma bolsa de dinheiro" />
+        <h1 className="font-medium text-4xl dark:text-white ml-4 pt-2">PJ x CLT</h1>
+      </div>
       <h1 className="text-red-600 text-5xl font-bold">!!! WORK IN PROGRESS !!!</h1>
       <button className="p-2 rounded-xl hover:bg-gray-500 dark:hover:bg-yellow-100 dark:text-white transition"
         onClick={() => setDarkMode(!darkMode)}>
