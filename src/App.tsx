373ffe2df39b8d9a056cbcec08305534548e5669
erickrodrig <erick.rod1100@gmail.com>
2023-12-18 09:48:54 -0300

    remove experimental code

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,6 @@ import { ComparisonTable } from './components/ComparisonTable';
 import { useEffect } from 'react';
 
 export default function App() {
-  useEffect(() => {
-    document.documentElement.setAttribute("data-theme", localStorage.getItem("theme") || 'dark');
-  }, []);
-
   return (
     <>
       <div className='flex flex-col min-h-screen'>
